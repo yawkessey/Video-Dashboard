dc5248105c8a88ceb0a6e5e1ecc338bf06a260da
yawkessey <yawkesseyankomah@gmail.com>
2022-11-30 16:22:56 -0500

    Imported video + auth routes

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+//Import express library and create an express app
 const express = require("express");
-
 const app = express();
 
+// Import Routes
+const videoRoutes = require("./routes/video");
+const authRoutes = require("./routes/auth");
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Main route is working");
 });
 
-// Routes 
+// Routes
+app.use("/video", videoRoutes);
+app.use("/auth", authRoutes);
+
 // app.use () ,express.static /demo/index_dev.html checks the folder and searches for the file
+
+// Start the server and listen on port 3000
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running 🚀");
 });
