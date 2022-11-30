eb4e245b60397b66fa0b634f24c69780ee96012b
yawkessey <yawkesseyankomah@gmail.com>
2022-11-30 16:11:11 -0500

    Added to the basic template for front_end

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Routes 
+// app.use () ,express.static /demo/index_dev.html checks the folder and searches for the file
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
