69403d6e4a23fbd28142ddd24e6556e13f982e88
yawkessey <yawkesseyankomah@gmail.com>
2022-12-01 13:16:11 -0500

    Database now has indentation format

--- data/storage.js
+++ data/storage.js
@@ -3,21 +3,21 @@ const fs = require("fs");
 let loadDatabase = (db_connection, schema = {}) => {
   console.log("Loading database...");
   if (!fs.existsSync(db_connection)) {
-    fs.writeFileSync(db_connection, JSON.stringify(schema));
+    fs.writeFileSync(db_connection, JSON.stringify(schema, null, 2));
     console.log("Database created");
   }
 
   let model = require(db_connection);
-    console.log("Database loaded");
+  console.log("Database loaded");
   //Create db object
   //Add more functions to db object
-    //get and set functions
+  //get and set
   let db = {
     model: model,
     filename: db_connection,
     update: () => {
-        console.log("Updating database...");
-      fs.writeFileSync(db_connection, JSON.stringify(model));
+      console.log("Updating database...");
+      fs.writeFileSync(db_connection, JSON.stringify(model, null, 2));
     },
     addCollection: (collection) => {
       model[collection] = [];
