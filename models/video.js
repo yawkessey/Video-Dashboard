ccb137dbd1c1025f4cfcec565dba58591bd88635
yawkessey <yawkesseyankomah@gmail.com>
2022-12-06 11:53:45 -0500

    Added in line comments and basic checkpoints

--- models/video.js
+++ models/video.js
@@ -1,11 +1,13 @@
 class Video {
     // Add a new video-share accessible to authenticated users only
     addVideo(video) {
+        // If registered user 
         db.model.videos.push(video);
         db.update();
     }
 
     displayVideos() {  
+        // If registered user
         return db.model.videos;
     }
 }
--- routes/video.js
+++ routes/video.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Video = require("../models/video");
 
-// Define routes
-    // Add a new video-share
 
-    // Display a video Dashboard accesssible to authenticated users only
+// Add a new video-share
+router.get("/video/new_video", (req, res) => {
+    // If session then they can add a video
+    // else "You must login to access this account"
+});
+router.post("/video/new_video", (req, res) => {
+    // If session then they can add a video
+    // else "You must login to access this account"
+});
+// Display a video Dashboard accesssible to authenticated users only
+router.get("/video/dashboard/:videofilter", (req, res) =>{
+    // If session then they can add a video
+    // else "You must login to access this account"
+})
 
 
 module.exports = router;
