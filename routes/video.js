f3825c8a89d648be2fb7b26d986dd09166edb8e1
yawkessey <yawkesseyankomah@gmail.com>
2022-12-06 12:07:54 -0500

    Added Video dashboard view and displays in route

--- routes/video.js
+++ routes/video.js
@@ -2,21 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Video = require("../models/video");
 
-
 // Add a new video-share
-router.get("/video/new_video", (req, res) => {
-    // If session then they can add a video
-    // else "You must login to access this account"
+router.get("/new_video", (req, res) => {
+  // If session then they can add a video
+  // else "You must login to access this account"
 });
-router.post("/video/new_video", (req, res) => {
-    // If session then they can add a video
-    // else "You must login to access this account"
+router.post("/new_video", (req, res) => {
+  // If session then they can add a video
+  // else "You must login to access this account"
 });
 // Display a video Dashboard accesssible to authenticated users only
-router.get("/video/dashboard/:videofilter", (req, res) =>{
-    // If session then they can add a video
-    // else "You must login to access this account"
-})
-
+router.get("/dashboard/:videofilter", (req, res) => {
+  // If session then they can add a video
+  // else "You must login to access this account"
+  res.render("video_dashboard");
+  let videos = Video.displayVideos();
+  res.send(videos);
+});
+router.get("/dashboard", (req, res) => {
+  // If session then they can add a video
+  // else "You must login to access this account"
+  res.render("video_dashboard");
+  let videos = Video.displayVideos();
+  res.send(videos);
+});
 
 module.exports = router;
