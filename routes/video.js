ad08d10fb0bfe9e0f38cb06a8a9408b12fc3cd63
yawkessey <yawkesseyankomah@gmail.com>
2022-12-06 16:48:31 -0500

    Routes render respective pages

--- routes/video.js
+++ routes/video.js
@@ -5,10 +5,15 @@ const Video = require("../models/video");
 // Add a new video-share
 router.get("/new_video", (req, res) => {
   // If session then they can add a video
+  model_view = {
+    action_url: "/add_video",
+  };
+  res.render("add_video.pug");
   // else "You must login to access this account"
 });
 router.post("/new_video", (req, res) => {
   // If session then they can add a video
+  Video.addVideo(req.body);
   // else "You must login to access this account"
 });
 // Display a video Dashboard accesssible to authenticated users only
@@ -22,9 +27,11 @@ router.get("/dashboard/:videofilter", (req, res) => {
 router.get("/dashboard", (req, res) => {
   // If session then they can add a video
   // else "You must login to access this account"
-  res.render("video_dashboard");
   let videos = Video.displayVideos();
-  res.send(videos);
+  console.log("Videos in dashboard:", videos)
+  res.render("video_dashboard", {videos});
 });
 
 module.exports = router;
+
+//     //- <iframe width="389" height="692" src="https://www.youtube.com/embed/aBtKnxy5mDg" title="Stop doing this on airplanes… #shorts" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
