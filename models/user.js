ba4056c18acf883f2273c9c6a020139dce855896
yawkessey <yawkesseyankomah@gmail.com>
2022-12-06 11:51:22 -0500

    Implemented user login/register logic

--- models/user.js
+++ models/user.js
@@ -1,12 +1,39 @@
 class User {
+  static fetchUserByEmail(email) {
+    // Fetch user from database
+    let user = db.model.users.find((user) => user.email === email);
+    return user;
+  }
 
-    static register(credentials) {
-        // Register user
+  static register(credentials) {
+    // Check if user exists in our database
+    const existing_user = this.fetchUserByEmail(credentials.email);
+    // Create new user in our database
+    if (!existing_user) {
+      db.model.users.push(credentials);
+      db.update();
+      return credentials;
+    } else {
+      console.log("User already exists. Log in.");
     }
+  }
 
-    static login(credentials) {
-        // Login user
+  static login(credentials) {
+    // Check if user exists in our database
+    const user = this.fetchUserByEmail(credentials.email);
+    console.log("User logging in");
+    // Check if password is correct
+    if (user && user.password === credentials.password) {
+      console.log("Login successful");
     }
+    // Create a new session for the user
+
+    // Return the session
+  }
+
+  static logout(session) {
+    // Destroy the session
+  }
 }
 
-module.exports = User; 
+module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
+// Session middleware
+//Should I leave this here or should I make it global to other code
+
+const session = require("express-session");
+router.use(
+  session({
+    secret: "ThisIsSupposedToBeASecret",
+    saveUninitialized: true,
+    resave: false,
+    cookie: { maxAge: 60000 },
+  })
+);
+
+// Login Routes
 router.get("/login", (req, res) => {
   model_view = {
     action_url: "/login",
@@ -8,6 +23,11 @@ router.get("/login", (req, res) => {
   res.render("login.pug");
 });
 
+router.post("/login", (req, res) => {
+  User.login(req.body);
+});
+
+// Register Routes
 router.get("/register", (req, res) => {
   model_view = {
     action_url: "/register",
@@ -16,11 +36,24 @@ router.get("/register", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  const { name, email, password} = req.body;
-  res.send(
-    `Name: ${name} Email: ${email} Password: ${password}`
-  )
-  let errors = [];
+  let credentials = User.register(req.body);
+  if (credentials) {
+    res.render("account_created");
+  } else {
+    // ERROR HANDLING
+  }
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      res.send("You are logged out!");
+    }
+  });
 });
 
 module.exports = router;
