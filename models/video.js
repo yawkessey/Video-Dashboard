327e26695f42eede553a0e567acb0c908420b915
yawkessey <yawkesseyankomah@gmail.com>
2022-12-06 16:43:29 -0500

    "User's link converts to embeded link and pushed to database"

--- models/video.js
+++ models/video.js
@@ -1,15 +1,26 @@
 class Video {
-    // Add a new video-share accessible to authenticated users only
-    addVideo(video) {
-        // If registered user 
-        db.model.videos.push(video);
-        db.update();
-    }
+  static extractVideoId(video_url) {
+    var regExp =
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|shorts\/)([^#&?]*).*/;
+    var match = video_url.match(regExp);
 
-    displayVideos() {  
-        // If registered user
-        return db.model.videos;
-    }
+    return match && match[2].length === 11 ? match[2] : null;
+  }
+
+  // Add a new video-share accessible to authenticated users only
+  static addVideo(video) {
+    // If registered user
+    let embededLink =
+      "https://www.youtube.com/embed/" + this.extractVideoId(video.video_url);
+    console.log("embededLink", embededLink);
+    db.model.videos.push({ video_url: embededLink });
+    db.update();
+  }
+
+  static displayVideos() {
+    // If registered user
+    return db.model.videos;
+  }
 }
 
 module.exports = Video;
