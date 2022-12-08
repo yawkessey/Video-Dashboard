d5e248daa7fbbc0a83609d1cc5bf88a659a96911
yawkessey <yawkesseyankomah@gmail.com>
2022-12-08 17:07:01 -0500

    Added middleware to restrict access

--- routes/auth.js
+++ routes/auth.js
@@ -1,41 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Video = require("../models/video");
 
-// Session middleware
-//Should I leave this here or should I make it global to other code
+//Access user in other routes
+// const { user } = res.locals
+// Checks if the user is authenticated
+// When trying to access a private route
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect("/auth/login");
+  } else {
+    next();
+  }
+};
 
-const session = require("express-session");
-router.use(
-  session({
-    secret: "ThisIsSupposedToBeASecret",
-    saveUninitialized: true,
-    resave: false,
-    cookie: { maxAge: 60000 },
-  })
-);
+// Redirects to home if they are already logged in
+const redirectHome = (req, res, next) => {
+  if (req.session.userId) {
+    console.log("Redirect home", req.session.userId);
+    res.redirect("/video/dashboard");
+  } else {
+    console.log("User is not authenticated");
+    next();
+  }
+};
 
 // Login Routes
-router.get("/login", (req, res) => {
-  model_view = {
-    action_url: "/login",
-  };
+router.get("/login", redirectHome, (req, res) => {
   res.render("login.pug");
 });
 
-router.post("/login", (req, res) => {
-  User.login(req.body);
+router.post("/login", redirectHome, (req, res) => {
+  const user = User.login(req.body);
+  if (user) {
+    req.session.userId = user.name;
+    req.session.isAuthenticated = true;
+    res.redirect("/video/dashboard");
+  } else {
+    res.redirect("/auth/login");
+  }
 });
 
 // Register Routes
-router.get("/register", (req, res) => {
-  model_view = {
-    action_url: "/register",
-  };
+router.get("/register", redirectHome, (req, res) => {
   res.render("register.pug");
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", redirectHome, (req, res) => {
   let credentials = User.register(req.body);
   if (credentials) {
     res.render("account_created");
@@ -45,7 +57,7 @@ router.post("/register", (req, res) => {
 });
 
 // Logout
-router.get("/logout", (req, res) => {
+router.get("/logout", redirectLogin, (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       console.log(err);
--- routes/video.js
+++ routes/video.js
@@ -1,37 +1,58 @@
 const express = require("express");
 const router = express.Router();
 const Video = require("../models/video");
+const User = require("./auth");
+
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect("/auth/login");
+  } else {
+    next();
+  }
+};
+
+router.use((req, res, next) => {
+  const { userId } = req.session;
+  console.log("User Id:", userId);
+  if (userId) {
+    res.locals.user = db.model.users.find((user) => user.name === userId);
+    console.log("User:", res.locals.user);
+  }
+  next();
+});
 
 // Add a new video-share
-router.get("/new_video", (req, res) => {
-  // If session then they can add a video
-  model_view = {
-    action_url: "/add_video",
-  };
+router.get("/new_video", redirectLogin, (req, res) => {
   res.render("add_video.pug");
-  // else "You must login to access this account"
 });
-router.post("/new_video", (req, res) => {
-  // If session then they can add a video
-  Video.addVideo(req.body);
-  // else "You must login to access this account"
+
+router.post("/new_video", redirectLogin, (req, res) => {
+  console.log("User id in confirmation page:", res.locals.user);
+  Video.addVideo(req.body, res.locals.user);
+  res.render("add_video_confirmation.pug");
+  console.log("User id in confirmation page:", res.locals.user);
 });
+
 // Display a video Dashboard accesssible to authenticated users only
-router.get("/dashboard/:videofilter", (req, res) => {
-  // If session then they can add a video
-  // else "You must login to access this account"
-  res.render("video_dashboard");
-  let videos = Video.displayVideos();
+router.get("/dashboard/:videofilter", redirectLogin, (req, res) => {
+  // 2 possible routes
+  // Logged in user's videos. req.params = mine
+  // All videos req.params = all
+  if (req.params.videofilter === "mine") {
+    let videos = Video.displayUserVideos(res.locals.user.userId);
+    console.log("User Videos:", videos);
+    // res.render("video_dashboard", { videos});
+  } else {
+    res.render("video_dashboard");
+  }
+
+  let videos = Video.displayAllVideos();
   res.send(videos);
 });
-router.get("/dashboard", (req, res) => {
-  // If session then they can add a video
-  // else "You must login to access this account"
-  let videos = Video.displayVideos();
-  console.log("Videos in dashboard:", videos)
-  res.render("video_dashboard", {videos});
+
+router.get("/dashboard", redirectLogin, (req, res) => {
+  let videos = Video.displayAllVideos();
+  res.render("video_dashboard", { videos });
 });
 
 module.exports = router;
-
-//     //- <iframe width="389" height="692" src="https://www.youtube.com/embed/aBtKnxy5mDg" title="Stop doing this on airplanes… #shorts" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
